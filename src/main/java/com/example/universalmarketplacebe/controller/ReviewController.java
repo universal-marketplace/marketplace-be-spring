62afce04543a4d701ecd9dc69a7848337d173a07
Banderan <bartoszwojcik2004@gmail.com>
2026-03-28 14:09:03 +0100

    adding notes to Controllers

--- src/main/java/com/example/universalmarketplacebe/controller/ReviewController.java
+++ src/main/java/com/example/universalmarketplacebe/controller/ReviewController.java
@@ -3,12 +3,13 @@ package com.example.universalmarketplacebe.controller;
 import com.example.universalmarketplacebe.dto.reviewRequest.ReplyRequest;
 import com.example.universalmarketplacebe.dto.reviewRequest.ReviewCreateRequest;
 import com.example.universalmarketplacebe.dto.reviewResponse.ReviewDto;
-import com.example.universalmarketplacebe.model.Reply;
-import com.example.universalmarketplacebe.model.Review;
 import com.example.universalmarketplacebe.service.reviewService.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Kontroler zarządzający opiniami (reviews) i odpowiedziami na nie.
+ */
 @RestController
 @RequestMapping("/api/reviews")
 @RequiredArgsConstructor
@@ -16,11 +17,41 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    /**
+     * Zalogowany użytkownik wystawia nową opinię drugiemu sprzedawcy/kupującemu.
+     * Używane np. po finalizacji transakcji.
+     *
+     * @param request DTO z id ocenianego użytkownika, gwiazdkami (1-5) i treścią.
+     * <br>Przykładowy Payload (żądanie z body w trybie JSON - warto tu dodać \@RequestBody):
+     * <pre>
+     * {
+     *   "targetUserId": 2,
+     *   "rating": 5,
+     *   "comment": "Bardzo szybka wysyłka, kontakt pierwsza klasa!"
+     * }
+     * </pre>
+     * @return Zapisany rekord w formie ReviewDto.
+     */
     @PostMapping
-    public ReviewDto createReview(ReviewCreateRequest request) {
+    public ReviewDto createReview(@RequestBody ReviewCreateRequest request) {
         return reviewService.createReview(request);
     }
 
+    /**
+     * Odpowiadanie na cudzą recenzję własnego profilu.
+     * Użytkownik, który otrzymał recenzję, dodaje do niej tzw. "reply".
+     *
+     * @param id ID docelowej recenzji.
+     * @param replyRequest Treść odpowiedzi.
+     * <br>Przykładowe wywołanie: {@code POST /api/reviews/10/reply}
+     * <br>Przykładowy Payload:
+     * <pre>
+     * {
+     *   "reply": "Dziękuję za zakupy, polecam się na przyszłość!"
+     * }
+     * </pre>
+     * @return Zaktualizowane ReviewDto, które zagnieżdża w sobie najnowszą odpowiedź.
+     */
     @PostMapping("/{id}/reply")
     public ReviewDto replyToReview(@PathVariable Long id, @RequestBody ReplyRequest replyRequest) {
         return reviewService.replyToReview(id, replyRequest);
